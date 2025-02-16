801e9df0a73ea7bdac5334ea8e7948ebbaa529c6
Afonso-eng <afonsomartinsb@gmail.com>
2025-02-16 17:52:18 -0300

    grafico_poo

---
+++ grafico_poo.py
@@ -0,0 +1,9 @@
+# grafico_poo.py
+import matplotlib.pyplot as plt
+meses=['jan','fev','mar','abr','mai','jun','jul','ago','set','out','nov','dez']
+vendas=[1500,1727,1350,1775,1052,1200,1300,1200,1100,1400,1500,1800]
+plt.xlabel('Meses')
+plt.ylabel('Vendas')
+plt.title('Vendas por mês')
+plt.plot(meses,vendas)
+plt.show()
